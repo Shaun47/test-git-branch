e16e2efd3f92a79c4c9d91ae9d841813026e37ab
Shaun47 <you@example.com>
2022-06-01 22:34:49 +0600

    first upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>working</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  mounted: function () {
+    console.log("shaun")
+    this.test()
+  },
+  methods: {
+    test(){
+      console.log("shaun hossain") 
+    }
+  }
+  
+}
+
+
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/TestTem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <h1>working test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestTem',
+
+  mounted: function () {
+
+  },
+  methods: {
+
+  }
+  
+}
+
+
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
