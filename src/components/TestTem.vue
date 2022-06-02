f00bcf4080ee6c4647221d8e1baef7eca1858a15
Shaun47 <you@example.com>
2022-06-02 07:26:35 +0600

    created 2nd argument

--- src/components/TestTem.vue
+++ src/components/TestTem.vue
@@ -1,13 +1,26 @@
 <template>
   <div >
-    <h1>working test</h1>
+    <!-- <h1>working test</h1> -->
+    <TestTwo title="checking props" summation="50"></TestTwo> 
+
+    <button @click="showpopup = true">Show Pop up</button>
   </div>
 </template>
 
 <script>
+import TestTwo from './TestTwo.vue'
+
+
 export default {
   name: 'TestTem',
-
+  components: {
+    TestTwo,
+},
+data(){
+  return{
+    showpopup : false
+  }
+},
   mounted: function () {
 
   },
---
+++ src/components/TestTwo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div >
+    <h1>working test 2 {{title}} {{summation}}</h1>
+    <p>random message starts</p>
+    <button>close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestTwo',
+  props:{
+    title:String,
+    summation: Number,
+
+  },
+  mounted: function () {
+
+  },
+  methods: {
+
+  }
+  
+}
+
+
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
